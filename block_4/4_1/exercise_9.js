6a9708d9a6ce9077c768dc2c23640e872ee00e90
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-13 19:23:09 -0300

    Exercise 9 done

---
+++ block_4/4_1/exercise_9.js
@@ -0,0 +1,11 @@
+let num1 = 12;
+let num2 = 8;
+let num3 = 10;
+let resposta = null;
+
+if (num1 % 2 === 1 || num2 % 2 === 1 || num3 % 2 === 1){
+    resposta = true;
+}else {
+    resposta = false;
+}
+console.log(resposta);
