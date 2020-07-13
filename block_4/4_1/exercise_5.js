236ca49f15062e0fb02f7dc2844499eb3467d226
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-13 18:42:18 -0300

    Exercise 5 done.

---
+++ block_4/4_1/exercise_5.js
@@ -0,0 +1,8 @@
+let angulo1 = 90;
+let angulo2 = 70;
+let angulo3 = 19;
+let soma = angulo1+angulo2+angulo3;
+let triangulo = (soma === 180) ? true : false;
+if (triangulo === false){
+    console.log("Erro: Esses angulos não formam um triangulo!")
+}
