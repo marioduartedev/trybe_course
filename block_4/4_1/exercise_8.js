5583c1e2d26b4998f389f0d678ebba3695826323
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-13 19:21:38 -0300

    Exercise 8 done

---
+++ block_4/4_1/exercise_8.js
@@ -0,0 +1,11 @@
+let num1 = 12;
+let num2 = 3;
+let num3 = 5;
+let resposta = null;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0){
+    resposta = true;
+}else {
+    resposta = false;
+}
+console.log(resposta);
