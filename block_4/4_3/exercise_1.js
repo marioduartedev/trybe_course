25c0149c8d3fd14f5ea631d4814f77c77c95e8b3
MarioDuarte <marioduarte.engenheiro@gmail.com>
2020-07-15 13:34:33 -0300

    Exercise 2 done

--- block_4/4_3/exercise_1.js
+++ block_4/4_3/exercise_1.js
@@ -2,7 +2,7 @@ let n = 5;
 let linha = "*";
 if (n > 1){
     for (let i = 1; i < n; i++){
-        linha += "*"
+        linha += "*";
     }
     for (let j = 1; j <= n; j++){
         console.log(linha);
